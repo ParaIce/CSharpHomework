e275951e8e8b255726fbf162542ce54a1e21a17a
ParaIce <well841117@163.com>
2018-09-12 19:33:25 +0800

    first commit

---
+++ homework1/homework1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, b, c;
+			Console.WriteLine("请输入两个整数：");
+			a = int.Parse(Console.ReadLine());
+			b = int.Parse(Console.ReadLine());
+			c = a * b;
+			Console.WriteLine("得到的乘积为{0}", c);
+		}
+	}
+}
